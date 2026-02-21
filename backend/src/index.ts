e8daa82bd53d9fc818bd847f848a9da58e11602b
KwakuYostOfficial <jordanpinto280@gmail.com>
2026-02-21 23:36:39 +0000

    feat: enhance user authentication adn synchronization with clerk; add axios and react-query for API handling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,11 +10,16 @@ import commentRoutes from "./routes/commentRoutes";
 
 const app = express();
 
+const corsOption = {
+  origin: ENV.FRONTEND_URL,
+  credentials: true, // credentials: true allows the frontend to send cookies to the backend so that we can authenticate the users
+};
+
 /* || MIDDLEWEARE SECTIONS */
 app.use(clerkMiddleware()); // auth object will be available in req.auth
 app.use(express.json()); // This parses JSON reques bodies.
 app.use(express.urlencoded({ extended: true })); // This parses form data like HTML fors.
-app.use(cors({ origin: ENV.FRONTEND_URL })); //This allows other domains access resources from this endpoint without being blocked
+app.use(cors(corsOption)); //This allows other domains access resources from this endpoint without being blocked
 
 app.get("/", (req, res) => {
   res.json({
