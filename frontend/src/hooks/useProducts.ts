062ccca549f266b434f7e3c778718713bd995303
KwakuYostOfficial <jordanpinto280@gmail.com>
2026-02-24 13:41:03 +0000

    feat: add CommentsSection component and hooks for creating and deleting comments; enhance ProductPage and Profile Page with comments functionality"

--- frontend/src/hooks/useProducts.ts
+++ frontend/src/hooks/useProducts.ts
@@ -1,8 +1,36 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
-import { createProduct, getAllProducts } from "../lib/api";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+  createProduct,
+  deleteProduct,
+  getAllProducts,
+  getMyProducts,
+  getProductById,
+} from "../lib/api";
 
 export const useProducts = () =>
   useQuery({ queryKey: ["products"], queryFn: getAllProducts });
 
 export const useCreateProduct = () =>
   useMutation({ mutationFn: createProduct });
+
+export const usedProduct = (id: string) =>
+  useQuery({
+    queryKey: ["product", id],
+    queryFn: () => getProductById(id),
+    enabled: !!id,
+    //double bang opperator
+  });
+
+export const useDeleteProduct = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: deleteProduct,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["myProducts"] });
+    },
+  });
+};
+
+export const useMyProducts = () => {
+  return useQuery({ queryKey: ["myProducts"], queryFn: getMyProducts });
+};
